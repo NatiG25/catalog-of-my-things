24e157fba8a052f86f16da7ed3b832b155d38cc5
NatiG <nati.g5684@gmail.com>
2022-08-10 00:50:19 +0300

    Create a has many between genre & items

--- item.rb
+++ item.rb
@@ -1,12 +1,17 @@
 # item class
 class Item
-  attr_accessor :genre, :author, :label
-  attr_reader :publish_date
+  attr_accessor :author, :label
+  attr_reader :publish_date, :genre
 
   def initialize(publish_date, archived: true)
     @id = Random.rand(1..1000)
     @archived = archived
     @publish_date = publish_date
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
   end
 
   def can_be_archived?
