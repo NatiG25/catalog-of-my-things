e8d7f290a9993d565fc0522dfa3b01538ab41081
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-09 21:39:58 +0500

    Create book class

---
+++ book/book.rb
@@ -0,0 +1,12 @@
+require_relative '../item'
+
+class Book < Item
+    def initialize(publisher, cover_state)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+
+    def can_be_archived?
+        super || cover_state == 'bad'
+    end
+end
