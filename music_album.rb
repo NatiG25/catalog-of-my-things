7ddeb9b5d4410096ec24142e5c6eef4714518e92
NatiG <nati.g5684@gmail.com>
2022-08-10 11:55:25 +0300

    Add methods to MusicAlbum

--- music_album.rb
+++ music_album.rb
@@ -1,2 +1,12 @@
-class MusicAlbum
+require_relative './item'
+
+class MusicAlbum < Item
+  def initialize(publish_date, on_spotify: true)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    can_be_archived? && @on_spotify
+  end
 end
