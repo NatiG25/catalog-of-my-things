b9884c127c2e878cb37f0466ee05f252d133e9c7
GASOMINALI Olivier <ogasominali@gmail.com>
2022-08-11 14:05:02 +0200

    Add Author and Game schema

--- schema.sql
+++ schema.sql
@@ -20,3 +20,17 @@ CREATE TABLE label(
     title VARCHAR(50)
     color VARCHAR(50)
 )
+
+CREATE TABLE author (
+	id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20)
+);
+
+CREATE TABLE game(
+	id INT NOT NULL,
+	last_played_at DATE,
+	multiplayer BOOLEAN,
+	FOREIGN KEY (id) REFERENCES item (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
